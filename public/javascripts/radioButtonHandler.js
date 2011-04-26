68eeeede292fb64f567119015195dbf9c6b568b6
paulgrenning <paul.grenning@gmail.com>
2011-04-26 02:47:21 -0500

    added javascript to handle validations

--- public/javascripts/radioButtonHandler.js
+++ public/javascripts/radioButtonHandler.js
@@ -9,16 +9,13 @@ $(document).ready( function(){
     $(this).attr("selected", "1");
     $(this).css("border-color", "#CF9020");
     $(this).css("background-color", "#DBBC5D");
-    /*
-    $(this).css("border-color", "#006666");
-    $(this).css("background-color", "#669999");
-    */
+    $("#errorset"+setNumber).fadeOut('slow', function (){}); 
   });
 
   $(".radiobutton").hover(function (){
     if($(this).attr("selected") == "0"){
-      $(this).css("border-color", "#669900");
-      $(this).css("background-color", "#99CC66");
+      $(this).css("border-color", "#006666");
+      $(this).css("background-color", "#669999");
     }
   });
 
@@ -29,3 +26,19 @@ $(document).ready( function(){
     }
   });
 });
+
+function submitSurveyJS(){
+ var numQuestions = parseInt($("#numberOfQuestions").attr("value"));
+ var retVal = true;
+ for(i = 0; i < numQuestions; i++){
+   for(j = 1; j < 6; j++){
+     if($("#radioset"+i+"index"+j).attr("selected") == "1"){
+       break; 
+     } else if(j == 5){
+       retVal = false;
+       $("#errorset"+i).fadeIn('slow', function (){}); 
+     }
+   }  
+ }
+ return retVal;
+}
